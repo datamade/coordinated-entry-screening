97562fe2873b9093fdb83a1df62c7a380f097c3e
reginafcompton <reginafcompton@gmail.com>
2018-08-02 16:37:47 -0500

    Update rapidsms template for Django 1.11

---
+++ csh/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from rapidsms import views as rapidsms_views
+
+admin.autodiscover()
+
+urlpatterns = (
+    url(r'^admin/', admin.site.urls),
+
+    # RapidSMS core URLs
+    url(r'^accounts/', include('rapidsms.urls.login_logout')),
+    url(r'^$', rapidsms_views.dashboard, name='rapidsms-dashboard'),
+
+    # RapidSMS contrib app URLs
+    url(r'^httptester/', include('rapidsms.contrib.httptester.urls')),
+    url(r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
+    url(r'^messaging/', include('rapidsms.contrib.messaging.urls')),
+    url(r'^registration/', include('rapidsms.contrib.registration.urls')),
+
+    # Third party URLs
+    url(r'^selectable/', include('selectable.urls')),
+)
