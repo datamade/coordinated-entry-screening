d4d4454511db8fb13727fcbe2e7b7ae3fb46240d
reginafcompton <reginafcompton@gmail.com>
2018-08-15 10:27:02 -0500

    Tell manage.py about app rename

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ces.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coordinated-entry-screening.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
