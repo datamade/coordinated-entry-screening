22035bcf9adc1ae5aff010234364afc27f7b381c
reginafcompton <reginafcompton@gmail.com>
2018-10-24 15:58:13 -0500

    Add open sessions to admin dashboard

--- ces_admin/views.py
+++ ces_admin/views.py
@@ -1,9 +1,14 @@
+from collections import namedtuple
+
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
+from django.db import connection
+
+from decisiontree.models import Session
 
 
 def ces_login(request):
@@ -24,4 +29,22 @@ def ces_logout(request):
 
 @login_required(login_url='/ces-login/')
 def ces_admin(request):
-    return render(request, 'ces_admin/ces-admin.html')
+    with connection.cursor() as cursor:
+        query_open_sessions ='''
+            SELECT session.start_date, session.last_modified, message.text
+            FROM decisiontree_session as session
+            JOIN decisiontree_treestate as state
+            ON session.state_id=state.id
+            JOIN decisiontree_message as message
+            ON state.message_id=message.id 
+            WHERE session.canceled is not True
+        '''
+        
+        cursor.execute(query_open_sessions)
+        fields = cursor.description
+        nt_result = namedtuple('Session', [col[0] for col in fields])
+        open_sessions = [nt_result(*row) for row in cursor.fetchall()]
+
+    return render(request, 'ces_admin/ces-dashboard.html', {
+            "open_sessions": open_sessions,
+        })
