1f69d689a3bbd7af65b5a2c4afa882cae733139a
reginafcompton <reginafcompton@gmail.com>
2018-11-01 12:15:01 -0500

    Add loads of fixtures

--- tests/test_admin_tools.py
+++ tests/test_admin_tools.py
@@ -4,29 +4,34 @@ from django.urls import reverse
 
 from ces_admin.utils import prepare_data
 
-def _test_dashboard_helper(auth_client):
-    url = reverse('dashboard')
-    response = auth_client.get(url)
+def test_open_sessions(auth_client, session):
+    response = _get_dashboard_helper(auth_client)
 
-    assert response.status_code == 200
+    session = session.build()
 
-    return response
+    import pdb
+    pdb.set_trace()
 
-def test_open_sessions(auth_client):
-    response = _test_dashboard_helper(auth_client)
-
-    assert response.context['open_sessions'] == 0
+    assert response.context['open_sessions'] == 1
 
 def test_canceled_sessions(auth_client):
-    response = _test_dashboard_helper(auth_client)
+    response = _get_dashboard_helper(auth_client)
 
     assert response.context['canceled_sessions'] == 0
 
 def test_completed_sessions(auth_client):
-    response = _test_dashboard_helper(auth_client)
+    response = _get_dashboard_helper(auth_client)
 
     assert response.context['completed_sessions'] == 0
 
 # TODO: create session fixtures for testing! 
 # Test the count for each type of session, chart data, and mapping data.
 # Add pytest to travis
+
+def _get_dashboard_helper(auth_client):
+    url = reverse('dashboard')
+    response = auth_client.get(url)
+
+    assert response.status_code == 200
+
+    return response
