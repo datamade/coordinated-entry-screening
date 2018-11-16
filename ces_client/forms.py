a20ac00c78ec84777043850c3257dac8b17a7f81
reginafcompton <reginafcompton@gmail.com>
2018-11-16 09:36:30 -0600

    Cleanup code: remove comments, add JS lib, and do not use CDNs

--- ces_client/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class ResponseForm(forms.Form):
-    response = forms.CharField(label='response', max_length=100)
--- ces_client/views.py
+++ ces_client/views.py
@@ -14,8 +14,6 @@ from decisiontree import conf
 from decisiontree.models import Session 
 from decisiontree.app import App
 
-from .forms import ResponseForm
-
 
 class IndexView(View):
     template_name = "ces_client/index.html"
@@ -27,9 +25,7 @@ class IndexView(View):
         request.session.flush()
         request.session.cycle_key()
 
-        form = ResponseForm()
-
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
         decision_app = App(router=MockRouter())
@@ -37,14 +33,11 @@ class IndexView(View):
         identity = 'web-{}'.format(request.session.session_key)
         connection, _ = Connection.objects.get_or_create(identity=identity, backend=backend)
 
-        form = ResponseForm(request.POST)
         message_from_ben = None
-
 
         user_input = request.POST.get('user_input')
         if user_input:
             msg = IncomingMessage(text=user_input, connection=connection)
-
             # The app evaluates to True (if it can send a message), or False (if it does not).
             # If False, then the user did not enter valid input, i.e., a trigger word or a pre-defined answer.
             if decision_app.handle(msg):
@@ -58,27 +51,6 @@ class IndexView(View):
             else:
                 message_from_ben = 'I am sorry. I do not understand.'
 
-
-
-        # if form.is_valid():
-        #     response = form.cleaned_data['response']
-        #     msg = IncomingMessage(text=response, connection=connection)
-
-        #     # The app evaluates to True (if it can send a message), or False (if it does not).
-        #     # If False, then the user did not enter valid input, i.e., a trigger word or a pre-defined answer.
-        #     if decision_app.handle(msg):
-        #         sessions = msg.connection.session_set.all().select_related('state')
-        #         session = sessions.latest('start_date')
-        #         state = session.state 
-        #         if not state:
-        #             state = session.state_at_close 
-
-        #         message_from_ben = decision_app._concat_answers(state.message.text, state)
-        #     else:
-        #         message_from_ben = 'I am sorry. I do not understand.'
-
-        # return render(request, self.template_name, {'form': form, 
-        #                                             'message_from_ben': message_from_ben})
         return HttpResponse(
             json.dumps({"text": message_from_ben}),
             content_type="application/json"
