bd1bd6c89ef62f3a5a6c234e8aa981ad988cce8c
Jasmine Mithani <jazzmithani@gmail.com>
2018-11-19 16:53:01 -0600

    initial stacked bar charts up and running

--- ces_admin/utils.py
+++ ces_admin/utils.py
@@ -1,10 +1,16 @@
 from collections import namedtuple
 import json
 
-def prepare_data(cursor, query):
+def prepare_data(cursor, query, percent_bool=True):
     cursor.execute(query)
     data = cursor.fetchall()
-    data_for_chart = [{'name': tup[0], 'y': tup[1]} for tup in data]
+
+    if percent_bool is True:
+        total = sum(tup[1] for tup in data)
+        data_for_chart = [{'name': tup[0], 'data': [100*(tup[1]/total)]} for tup in data]
+    else:
+        data_for_chart = [{'name': tup[0], 'y': tup[1]} for tup in data]
+
     mapping = {tup[0]: tup[2] for tup in data}
 
     return json.dumps(data_for_chart), json.dumps(mapping)
--- coordinated-entry-screening/static/js/dashboard-charts.js
+++ coordinated-entry-screening/static/js/dashboard-charts.js
@@ -19,7 +19,7 @@ function barHelper(container, prepped_data, data_map){
         },
         yAxis: {
             title: {
-                text: 'Number of users'
+                text: 'Number of times resource was recommended'
             },
             allowDecimals: false
         },
@@ -50,5 +50,62 @@ function barHelper(container, prepped_data, data_map){
                 "data": prepped_data
             }
         ]
+    });
+}
+
+function stackedBarHelper(container, prepped_data, data_map){
+    Highcharts.chart(container, {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: null
+        },
+        subtitle: {
+            text: null
+        },
+        xAxis: {
+          labels: {
+            enabled: false
+          },
+          tickWidth: 0
+        },
+        yAxis: {
+            title: {
+                text: 'Percent of users ending on result'
+            },
+            allowDecimals: false
+        },
+        legend: {
+            enabled: true
+        },
+        plotOptions: {
+            series: {
+              stacking: 'percent',
+              dataLabels: {
+                enabled: true,
+                format: '{y:,.0f}%',
+                color: '#fff',
+                style: {
+                  textShadow: 'none',
+                  opacity: .8,
+                  fontWeight: 'bold'
+                }
+              }
+            }
+        },
+        tooltip: {
+            backgroundColor: 'rgba(236, 240, 241, .85)',
+            style: {
+                color: '#3B4B5C',
+            },
+            useHTML: true,
+            headerFormat: '',
+            pointFormat:  '<h6><strong>{point.y} users</strong></h6><hr><p>{point.name}</p>',
+            shared: true,
+            shadow: false,
+            borderColor: '#3B4B5C'
+        },
+        series: prepped_data
     });
 }
