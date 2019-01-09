b89c2c00d46df15875318dcd883fefa25dd790a5
Jasmine Mithani <jazzmithani@gmail.com>
2019-01-09 15:50:28 -0600

    making B.E.N. a little more mobile friendly

--- coordinated-entry-screening/static/js/bot-utils.js
+++ coordinated-entry-screening/static/js/bot-utils.js
@@ -1,14 +1,14 @@
-/* 
+/*
 The following helper functions facilitate chat with B.E.N.
 
-@sendAjax manages communication between the client and the Django view. 
+@sendAjax manages communication between the client and the Django view.
 It accepts user input (i.e., the response to a survey question - the value of "result")
-and sends the input to the Index view via a POST request. 
+and sends the input to the Index view via a POST request.
 
-If the Index view return data (i.e., a message from B.E.N.), 
+If the Index view return data (i.e., a message from B.E.N.),
 then @sendAjax does the following:
 (1) adds this message to the chat
-(2) adds options for answers (or a "goodbye" message) and 
+(2) adds options for answers (or a "goodbye" message) and
 sets the value "result" (a global varible used to track user input)
 (3) auto-scrolls to the bottom of the chat window
 */
@@ -21,7 +21,7 @@ function sendAjax(botui, input, endMsg) {
         url: "/",
         data : { user_input: input },
 
-        success : function(data) {                
+        success : function(data) {
             botui.message.add({
                 html: true,
                 cssClass: 'bot-msg',
@@ -45,7 +45,7 @@ function sendAjax(botui, input, endMsg) {
 }
 
 function prettyBENMsg(content) {
-    return '<div class="row"><div class="col ben-sidebar"><img src="static/images/ben-icon-03.svg"></div><div class="col"><small><strong>B.E.N.</strong> <em>Bravely Engineered Navigator</em></small></br><div class="mt-2">' + content + '</div></div></div>';
+    return '<div class="row"><div class="col ben-sidebar"><img src="static/images/ben-icon-03.svg"></div><div class="col ben-words"><small><strong>B.E.N.</strong> <em>Bravely Engineered Navigator</em></small></br><div class="mt-2 ben-words-content">' + content + '</div></div></div>';
 }
 
 function getMessages() {
