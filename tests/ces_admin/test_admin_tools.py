1f0f652dbbfb136c7fc6e9fe140a297e7926bd36
reginafcompton <reginafcompton@gmail.com>
2019-01-25 17:03:45 -0500

    Write a test

--- tests/ces_admin/test_admin_tools.py
+++ tests/ces_admin/test_admin_tools.py
@@ -57,6 +57,29 @@ def test_completed_sms_sessions(auth_client, sms_connection, session):
     response = _get_dashboard_helper(auth_client)
     assert response.context['sms_completed_sessions'] == 1
 
+
+def test_tree_locations(auth_client, sms_connection, session, tree):
+    '''
+    Test that the admin view returns the correct list of surveys triggered by location codes:
+    zero surveys when only web sessions are present, and one result when a session triggers a 
+    survey with a location code.
+    '''
+    response = _get_dashboard_helper(auth_client)
+    assert len(response.context['survey_trees']) == 0 
+
+    tree.trigger = '0006'
+    tree.save()
+
+    session_info = {
+        'connection_id': sms_connection.id,
+        'tree_id': tree.id
+    }
+    session.build(**session_info)
+
+    response = _get_dashboard_helper(auth_client)
+    assert len(response.context['survey_trees']) == 1
+
+
 def _get_dashboard_helper(auth_client):
     url = reverse('dashboard')
     response = auth_client.get(url)
