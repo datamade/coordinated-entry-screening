2fbbc24af9bee279c173b3cab216643a682ffe45
reginafcompton <reginafcompton@gmail.com>
2019-06-29 10:55:58 -0500

    Add content to About and Contact pages

--- ces_client/urls.py
+++ ces_client/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from .views import IndexView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name="ces_client/about.html"), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name="ces_client/contact.html"), name='contact'),
 ]
